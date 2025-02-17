31e8a1ab711821b06ea8725d98a0aa45db5ee9f9
Sheena O'Connell <sheena.oconnell@gmail.com>
2025-02-17 21:52:01 +0200

    can register user for course

--- content_management/admin.py
+++ content_management/admin.py
@@ -1,11 +1,13 @@
 from . import models
 from django.contrib import admin
 from grappelli.forms import GrappelliSortableHiddenMixin
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-@admin.register(models.ContentItem)
-class ContentItemAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(models.ContentItem)
+# class ContentItemAdmin(admin.ModelAdmin):
+#     pass
 
 
 class CourseContentInline(GrappelliSortableHiddenMixin, admin.TabularInline):
@@ -21,3 +23,39 @@ class CourseContentInline(GrappelliSortableHiddenMixin, admin.TabularInline):
 @admin.register(models.Course)
 class CourseAdmin(admin.ModelAdmin):
     inlines = [CourseContentInline]
+
+
+class CourseAccessInline(admin.TabularInline):
+    model = models.CourseAccess
+    readonly_fields = ["datetime_created"]
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    inlines = [CourseAccessInline]
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "email",
+                    "full_name",
+                    "is_active",
+                ],
+            },
+        ),
+        (
+            "Permission options",
+            {
+                "classes": ["grp-collapse grp-closed"],
+                "fields": [
+                    "is_staff",
+                    "is_superuser",
+                    "user_permissions",
+                    "groups",
+                ],
+            },
+        ),
+    ]
+    readonly_fields = ["last_login"]
