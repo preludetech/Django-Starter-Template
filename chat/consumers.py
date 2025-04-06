4bc7d10dd3fa9021412cc7a31e0e0f28a382a4e7
Sheena O'Connell <sheena.oconnell@gmail.com>
2025-04-06 20:59:39 +0200

    chat messages get displayed on submit with ws

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,28 @@
 from channels.generic.websocket import WebsocketConsumer
+from .models import Conversation, Message
+from django.template.loader import render_to_string
+import json
 
 
 class ConversationConsumer(WebsocketConsumer):
     def connect(self):
+
+        self.user = self.scope["user"]
+        conversation_name = self.scope["url_route"]["kwargs"]["name"]
+
+        self.conversation = Conversation.objects.get(name=conversation_name)
         self.accept()
+
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        message_string = data["message"]
+        message = Message.objects.create(
+            message=message_string, conversation=self.conversation, from_user=self.user
+        )
+
+        print("message created")
+        context = {"message": message, "user": self.user}
+        html = render_to_string(
+            "chat/conversation.html#hx-chat-bubble", context=context
+        )
+        self.send(text_data=html)
--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import ConversationConsumer
 
 websocket_urlpatterns = [
-    path("ws/conversation/<str:name>", ConversationConsumer.as_asgi()),
+    path(r"ws/conversation/<str:name>/", ConversationConsumer.as_asgi()),
 ]
